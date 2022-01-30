f0c412b2aadf684d7f137af04de8b0eaca798506
Kate Lucas <katejalucas@gmail.com>
2022-01-30 18:51:33 +0000

    made an alphabetical list of just names

--- Task1F.py
+++ Task1F.py
@@ -1,15 +1,22 @@
+from sympy import inverse_cosine_transform
 from floodsystem.station import inconsistent_typical_range_stations
 from floodsystem.stationdata import build_station_list
 
 stations = build_station_list()
 
+
+
+inconsistent_stations = (inconsistent_typical_range_stations(stations))
+
 names = []
+for station in inconsistent_stations:
+    s = station.name
+    names.append(s)
 
-for station in stations:
-    data =station.name
-    names.append(data)
+alphabetical_names = sorted(names)
 
-inconsistent_stations = sorted(inconsistent_typical_range_stations(stations))
-print(inconsistent_stations)
+
+
+print(alphabetical_names)
 
 
