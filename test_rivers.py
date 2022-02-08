59ab9876238f220b93c30b68bf3fd77c8916d165
Sophie Langdon <sl2066@cam.ac.uk>
2022-02-08 10:01:09 +0000

    String edited to say 10

--- test_rivers.py
+++ test_rivers.py
@@ -6,11 +6,11 @@ def test_riverFunction():
     stations = build_station_list()
     return list(rivers_with_station(stations))
 
-print("Printing list of rivers",test_riverFunction()[0:10])
+print("Printing list of 10 rivers",test_riverFunction()[0:10])
 
 def test_riverDictFunction():
     stations = build_station_list()
     items = list(stations_by_river(stations).items())
     return items[0:10]
 
-print("Printing Dictionary of rivers with their stations",test_riverDictFunction())
+print("Printing Dictionary of 10 rivers with their stations",test_riverDictFunction())
