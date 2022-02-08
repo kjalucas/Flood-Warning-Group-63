b83370243cb74ab59377f534ffd9713a0a54b0df
Kate Lucas <katejalucas@gmail.com>
2022-02-08 00:18:00 +0000

    moved inconsistent function test and stations within radius to geo.py

--- test_geo.py
+++ test_geo.py
@@ -2,8 +2,11 @@ from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 from floodsystem.station import MonitoringStation
 from floodsystem.geo import stations_within_radius
+from floodsystem.station import inconsistent_typical_range_stations
 
 def test_stations_by_distance():
+
+    ## create a set of three imaginary stations
 
     s_id = "test-s-id"
     m_id = "test-m-id"
@@ -25,7 +28,12 @@ def test_stations_by_distance():
     coord3 = (3,3)
     c = MonitoringStation(s_id, m_id, label3, coord3, trange, river, town)
 
+    ## put the stations in a list
+
     stations = [a,c,b]
+
+    ## test that the function returns a list of objects in order a,b,c
+
     distances = stations_by_distance(stations, (0, 0))
     stations_and_distances = []
     for station, distance in distances:
@@ -37,6 +45,8 @@ def test_stations_by_distance():
 
 def test_stations_within_radius():
 
+    ## create a set of three imaginary stations
+
     s_id = "test-s-id"
     m_id = "test-m-id"
     trange = (-2.3, 3.4445)
@@ -57,7 +67,11 @@ def test_stations_within_radius():
     coord3 = (3,3)
     c = MonitoringStation(s_id, m_id, label3, coord3, trange, river, town)
 
+    ## put the stations in a list
+
     stations = [a,c,b]
+
+    ## checking how many stations are within a radius of 400 - should be stations 1 and 2 with radius 100 and 314 respectively
     in_radius = stations_within_radius(stations, (0,0), 400)
     stations_in_radius = []
 
@@ -66,6 +80,49 @@ def test_stations_within_radius():
         stations_in_radius.append(data)
         
     assert len(stations_in_radius) == 2 
+
+def test_inconsistent_typical_range_stations():
+
+    ## create a set of three imaginary stations
+
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    river = "River X"
+    town = "My Town"
+    
+
+
+    label1 = "Station 1"
+    coord1 = (1,1)
+    trange1 = (2, -2)
+    a = MonitoringStation(s_id, m_id, label1, coord1, trange1, river, town)
+    
+    label2 = "Station 2"
+    coord2 = (2,2)
+    trange2 = (3, 10)
+    b = MonitoringStation(s_id, m_id, label2, coord2, trange2, river, town)
+
+    label3 = "Station 3"
+    coord3 = (3, 3)
+    trange3 = (1, -1)
+    c = MonitoringStation(s_id, m_id, label3, coord3, trange3, river, town)
+
+    ## put the stations in a list
+
+    stations = [a,c,b]
+
+    ## tests function on stations and creates a list of inconsistent stations
+    inconsistent_stations = (inconsistent_typical_range_stations(stations))
+    names = []
+    for station in inconsistent_stations:
+        s = station.name
+        names.append(s)
+
+    ##check that names contains the two stations - 1 and 3 - that have inconsistent ranges
+
+    assert len(names) == 2
+
+
 
 
 
