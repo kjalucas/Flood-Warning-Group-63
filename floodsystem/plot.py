94dd9163dbe5077b687ae57fec60cd2834fb7da5
Kate Lucas <katejalucas@gmail.com>
2022-02-11 12:43:07 +0000

    wrote first go at plot water levels

---
+++ floodsystem/plot.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from . import datafetcher
+from .station import MonitoringStation
+from .stationdata import build_station_list
+from .station import typical_high_range
+
+def plot_water_levels(station, dates, levels):
+
+    # Plot
+    plt.plot(dates, levels)
+
+    #Plot low range
+    plt.plot([dates[0], dates [-1]], [station.typical_range[0], station.typical_range[0], label = "Typical Low Levels")
+
+    #Plot high range
+    plt.plot([dates[0], dates [-1]], [station.typical_range[1], station.typical_range[1], label = "Typical High Levels")
+
+
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('Date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title("Station " + station.name)
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
