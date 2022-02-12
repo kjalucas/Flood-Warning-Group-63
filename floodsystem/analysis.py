619e997bb77a802896647de29cb933b79e2bbd66
Kate Lucas <katejalucas@gmail.com>
2022-02-12 19:41:22 +0000

    wrote polyfit function

---
+++ floodsystem/analysis.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib as mpl
+from datetime import datetime, timedelta
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+import matplotlib.pyplot as plt
+
+def polyfit(dates, levels, p):
+    
+    days = matplotlib.dates.date2num(dates)
+    d0 = np.amin(days)
+    x = days - d0
+    y = levels
+
+    # Find coefficients of best-fit polynomial f(x) of degree 4
+    p_coeff = np.polyfit(x, y, p)
+    # Convert coefficient into a polynomial that can be evaluated,
+    # e.g. poly(0.3)
+    poly = np.poly1d(p_coeff)
+
+    return d0, poly
