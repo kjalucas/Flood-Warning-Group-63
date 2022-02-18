408577013178e76359ab89a25f1f3ab51ac61efe
Sophie Langdon <sl2066@cam.ac.uk>
2022-02-18 21:37:49 +0000

    Task 2B

---
+++ Task2B.py
@@ -0,0 +1,16 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_level_over_threshold
+
+
+def names_and_levels(tuples):
+    newList = []
+    for t in tuples:
+        newTuple = (t[0].name,t[1])    
+        newList.append(newTuple)
+    return newList    
+
+stations = build_station_list()
+update_water_levels(stations)
+s = stations_level_over_threshold(stations, 0.8)
+
+print(names_and_levels(s))
---
+++ floodsystem/flood.py
@@ -0,0 +1,19 @@
+#from floodsystem.station import MonitoringStation
+#from floodsystem.stationdata import build_station_list
+
+def stations_level_over_threshold(stations, tol):
+    '''Returns a list of tuples which gives a station where
+    the latest relative water level is over tol and that relative water level.
+    Sorted in descending order'''
+    overTol = [] # Empty list for tuples 
+    for station in stations:
+        if station.relative_water_level() == None:
+            pass
+        elif station.relative_water_level() > tol:
+            tuple1 = (station,station.relative_water_level())
+            overTol.append(tuple1)
+        overTol.sort(key=lambda x: x[1], reverse=True)
+    return overTol
+
+
+        
